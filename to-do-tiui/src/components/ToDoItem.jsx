cf427d4e6848b9958df76fc2f92fd7ba9bcd1b90
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-07-02 19:33:29 -0600

    ToDoItem was edited

--- to-do-tiui/src/components/ToDoItem.jsx
+++ to-do-tiui/src/components/ToDoItem.jsx
@@ -5,15 +5,20 @@ import Edit from "@mui/icons-material/edit";
 
 function ToDoItem() {
   return (
-    <div className=" bg-white w-full flex items-center rounded-md">
-      <Checkbox color="success" />
-      <p>ToDoItem</p>
-      <AppButton icon={<Edit />} color={"secondary"}>
-        Editar
-      </AppButton>
-      <AppButton icon={<Delete />} color={"error"}>
-        Eliminar
-      </AppButton>
+    <div className=" bg-white flex justify-between items-center rounded-md p-3 mx-5">
+      <section className=" flex items-center gap-1">
+        <Checkbox color="success" />
+        <p>Tarea 1</p>
+      </section>
+
+      <section className=" flex items-center gap-1">
+        <AppButton icon={<Edit />} color={"secondary"}>
+          Editar
+        </AppButton>
+        <AppButton icon={<Delete />} color={"error"}>
+          Eliminar
+        </AppButton>
+      </section>
     </div>
   );
 }
